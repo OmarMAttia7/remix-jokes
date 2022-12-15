799f56f0144f46ab9a656a983f034b8f49fdc5b0
hazel97 <omarmattia97@gmail.com>
2022-12-15 16:18:49 +0200

    feat: add stylesheets

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
 export default function indexRoute() {
   return <h1>Hello, Index Route</h1>;
 }
