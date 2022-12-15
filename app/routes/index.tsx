49a4c554b8ac3cb2d5daec0ff28ad6501f946462
hazel97 <omarmattia97@gmail.com>
2022-12-15 17:03:40 +0200

    feat: update home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,27 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
-import stylesUrl from "../styles/index.css";
+import stylesUrl from "~/styles/index.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function indexRoute() {
-  return <h1>Hello, Index Route</h1>;
+export default function IndexRoute() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
