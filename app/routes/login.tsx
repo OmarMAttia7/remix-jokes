979e4cbc53da6fcfc0960ebaab4e0dbcb13d4e20
hazel97 <omarmattia97@gmail.com>
2022-12-17 19:15:19 +0200

    feat: add authentication

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -12,7 +12,7 @@ import {
 import stylesUrl from "~/styles/login.css";
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
-import { login } from "~/utils/session.server";
+import { createUserSession, login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -75,7 +75,7 @@ export const action = async ({ request }: ActionArgs) => {
   switch (loginType) {
     case "login": {
       const user = await login({ username, password });
-      console.log({ user });
+
       if (!user) {
         return badRequest({
           fieldErrors: null,
@@ -84,11 +84,7 @@ export const action = async ({ request }: ActionArgs) => {
         });
       }
       // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fieldErrors: null,
-        fields,
-        formError: "Not implemented",
-      });
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
